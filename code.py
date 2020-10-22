3966cccd0e7372eaed102167a5bcbc0646348b3f
Ashok917786 <67753129+Ashok917786@users.noreply.github.com>
2020-10-22 22:04:44 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,6 @@
 # --------------
 from csv import reader
 
-
 def explore_data(dataset, start, end, rows_and_columns=False):
     """Explore the elements of a list.
     
@@ -13,16 +12,18 @@ def explore_data(dataset, start, end, rows_and_columns=False):
     end -- index of the stopping element, this is excluded 
     rows_and_columns -- this parameter is optional while calling the function. It takes binary          values, either True or False. If true, print the dimension of the list, else dont.
     """
+    
+    
     dataset_slice = dataset[start:end]    
     for row in dataset_slice:
         print(row)
         print('\n') # adds a new (empty) line between rows
-
+        
     if rows_and_columns:
         print('Number of rows:', len(dataset))
         print('Number of columns:', len(dataset[0]))
-
-
+        
+     
 
 
 def duplicate_and_unique_movies(dataset, index_):
@@ -36,7 +37,7 @@ def duplicate_and_unique_movies(dataset, index_):
     index_ -- column index at which the element in each row would be checked for duplicacy 
     
     """
-
+    
     duplicate = []
     unique = []
 
@@ -50,7 +51,7 @@ def duplicate_and_unique_movies(dataset, index_):
     print('Number of duplicate Movies:', len(duplicate))
     print('\n')
     print('Examples of duplicate Movies:', duplicate[:15])
-
+    
 
 
 
@@ -79,7 +80,7 @@ def movies_lang(dataset, index_, lang_):
     print("Examples of Movies in English Language:")    
     explore_data(movies_, 0, 3, True)
     return movies_
-
+    
 
 
 def rate_bucket(dataset, rate_low, rate_high):
@@ -154,13 +155,13 @@ reviews_max = {}
 for movie in movies:
     name = movie[13]
     n_reviews = float(movie[12])
-
+    
     if name in reviews_max and reviews_max[name] < n_reviews:
         reviews_max[name] = n_reviews
-
+        
     elif name not in reviews_max:
         reviews_max[name] = n_reviews
-
+        
 len(reviews_max)
 
 # Create a list 'movies_clean', which will filter out the duplicate movies and contain the rows with maximum number of reviews for duplicate movies, as stored in 'review_max'. 
@@ -171,11 +172,11 @@ already_added = []
 for movie in movies:
     name = movie[13]
     n_reviews = float(movie[12])
-
+    
     if (reviews_max[name] == n_reviews) and (name not in already_added):
         movies_clean.append(movie)
         already_added.append(name)
-
+        
 len(movies_clean)
 
 
@@ -189,117 +190,3 @@ movies_en = movies_lang(movies_clean, 3, 'en')
 # Call the rate_bucket function to see the movies with rating higher than 8.
 
 high_rated_movies = rate_bucket(movies_en, 8, 10)
-
-    
-        
-     
-
-
-def duplicate_and_unique_movies(dataset, index_):
-    """Check the duplicate and unique entries.
-    
-    We have nested list. This function checks if the rows in the list is unique or duplicated based     on the element at index 'index_'.
-    It prints the Number of duplicate entries, along with some examples of duplicated entry.
-    
-    Keyword arguments:
-    dataset -- two dimensional list which we want to explore
-    index_ -- column index at which the element in each row would be checked for duplicacy 
-    
-    """
-    
-    
-
-
-def movies_lang(dataset, index_, lang_):
-    """Extract the movies of a particular language.
-    
-    Of all the movies available in all languages, this function extracts all the movies in a            particular laguage.
-    Once you ahve extracted the movies, call the explore_data() to print first few rows.
-
-    Keyword arguments:
-    dataset -- list containing the details of the movie
-    index_ -- index which is to be compared for langauges
-    lang_ -- desired language for which we want to filter out the movies
-    
-    Returns:
-    movies_ -- list with details of the movies in selected language
-    
-    """
-    
-    return movies_
-    
-
-
-
-def rate_bucket(dataset, rate_low, rate_high):
-    """Extract the movies within the specified ratings.
-    
-    This function extracts all the movies that has rating between rate_low and high_rate.
-    Once you ahve extracted the movies, call the explore_data() to print first few rows.
-    
-    Keyword arguments:
-    dataset -- list containing the details of the movie
-    rate_low -- lower range of rating
-    rate_high -- higher range of rating
-    
-    Returns:
-    rated_movies -- list of the details of the movies with required ratings
-    """
-
-    
-    return rated_movies
-
-
-
-
-# Read the data file and store it as a list 'movies'
-opened_file = open(path, encoding="utf8")
-read_file = reader(opened_file)
-movies = list(read_file)
-
-
-
-# The first row is header. Extract and store it in 'movies_header'.
-
-
-# Subset the movies dataset such that the header is removed from the list and store it back in movies
-
-
-
-
-# Delete wrong data
-
-# Explore the row #4553. You will see that as apart from the id, description, status and title, no other information is available.
-# Hence drop this row.
-
-
-
-# Using explore_data() with appropriate parameters, view the details of the first 5 movies.
-
-
-
-
-# Our dataset might have more than one entry for a movie. Call duplicate_and_unique_movies() with index of the name to check the same.
-
-
-
-
-# We saw that there are 3 movies for which the there are multiple entries. 
-# Create a dictionary, 'reviews_max' that will have the name of the movie as key, and the maximum number of reviews as values.
-
-
-
-# Create a list 'movies_clean', which will filter out the duplicate movies and contain the rows with maximum number of reviews for duplicate movies, as stored in 'review_max'. 
-
-
-
-
-# Calling movies_lang(), extract all the english movies and store it in movies_en.
-
-
-
-
-# Call the rate_bucket function to see the movies with rating higher than 8.
-
-
-
